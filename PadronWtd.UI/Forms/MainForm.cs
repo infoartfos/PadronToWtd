feb451dc8476a203974244c69b5ee7e042b5d6e7
Carlos V <cvalicenti@artfos.com.ar>
2025-11-14 14:28:26 -0300

    Agregando Cliente SAP y Menu

---
+++ PadronWtd.UI/Forms/MainForm.cs
@@ -0,0 +1,79 @@
+﻿using SAPbouiCOM;
+// using PadronSaltaAddOn.Application.Services;
+
+namespace PadronSaltaAddOn.UI.Forms
+{
+    internal class MainForm
+    {
+        private readonly Application _app;
+        //     private readonly ImpuestoService _impuestoService;
+        private Form _form;
+
+        //        public MainForm(Application app, ImpuestoService impuestoService)
+        public MainForm(Application app)
+        {
+            _app = app;
+            //       _impuestoService = impuestoService;
+            CreateForm();
+        }
+
+        private void CreateForm()
+        {
+            FormCreationParams cp = (FormCreationParams)_app.CreateObject(BoCreatableObjectType.cot_FormCreationParams);
+            cp.UniqueID = "frmPadron";
+            cp.FormType = "frmPadron";
+            cp.BorderStyle = BoFormBorderStyle.fbs_Fixed;
+
+            _form = _app.Forms.AddEx(cp);
+            _form.Title = "ACTUALIZACION IMPOSITIVA *SALTA*";
+            _form.Width = 430;
+            _form.Height = 260;
+
+            Item label = _form.Items.Add("lblOpt", BoFormItemTypes.it_STATIC);
+            label.Top = 40; label.Left = 20;
+            ((StaticText)label.Specific).Caption = "Opciones:";
+
+            AddButton("btnFecha", "Mantenimiento de Fecha", 70);
+            AddButton("btnImp", "Mantenimiento de Impuestos", 110);
+            AddButton("btnProc", "Importar y procesar", 150);
+
+            _app.ItemEvent += App_ItemEvent;
+            _form.Visible = true;
+        }
+
+        private void AddButton(string id, string caption, int top)
+        {
+            Item btn = _form.Items.Add(id, BoFormItemTypes.it_BUTTON);
+            btn.Top = top; btn.Left = 40; btn.Width = 200;
+            ((Button)btn.Specific).Caption = caption;
+        }
+
+        private void App_ItemEvent(string FormUID, ref ItemEvent pVal, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+            if (pVal.EventType == BoEventTypes.et_ITEM_PRESSED && !pVal.BeforeAction && FormUID == "frmPadron")
+            {
+                switch (pVal.ItemUID)
+                {
+                    case "btnImp":
+                        _app.MessageBox("Tasa actualizada correctamente");
+                        break;
+                    case "btnProc":
+                        _app.MessageBox($"implementada: {pVal.ItemUID}");
+                        OnImportarClick();
+                        break;
+                    default:
+                        _app.MessageBox($"Acción no implementada: {pVal.ItemUID}");
+                        break;
+                }
+            }
+        }
+
+        private void OnImportarClick()
+        {
+            //var frmImportar = new FrmImportar(SBO_Application, oCompany);
+            //var frmImportar = new FrmImportar(_app);
+            //frmImportar.CreateForm();
+        }
+    }
+}
--- PadronWtd.UI/Program.cs
+++ PadronWtd.UI/Program.cs
@@ -1,43 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using SAPbouiCOM.Framework;
-using System;
 
 namespace PadronWtd.UI
 {
-class Program
-{
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main(string[] args)
+    class Program
     {
-        try
-        {
-            Application oApp = null;
-            if (args.Length < 1)
-            {
-                oApp = new Application();
-            }
-            else
-            {
-                //If you want to use an add-on identifier for the development license, you can specify an add-on identifier string as the second parameter.
-                //oApp = new Application(args[0], "XXXXX");
-                oApp = new Application(args[0]);
-            }
-            Menu MyMenu = new Menu();
-            MyMenu.AddMenuItems();
-            oApp.RegisterMenuEventHandler(MyMenu.SBO_Application_MenuEvent);
 
-            Application.SBO_Application.AppEvent += new SAPbouiCOM._IApplicationEvents_AppEventEventHandler(SBO_Application_AppEvent);
-            Application.SBO_Application.ItemEvent += SBO_Application_ItemEvent;
-            oApp.Run();
-        }
-        catch (Exception ex)
+        [STAThread]
+        static void Main(string[] args)
         {
-            System.Windows.Forms.MessageBox.Show(ex.Message);
+            try
+            {
+                Application oApp = null;
+                if (args.Length < 1)
+                {
+                    oApp = new Application();
+                }
+                else
+                {
+                    //If you want to use an add-on identifier for the development license, you can specify an add-on identifier string as the second parameter.
+                    //oApp = new Application(args[0], "XXXXX");
+                    oApp = new Application(args[0]);
+                }
+                Menu MyMenu = new Menu();
+                MyMenu.AddMenuItems();
+                oApp.RegisterMenuEventHandler(MyMenu.SBO_Application_MenuEvent);
+
+                Application.SBO_Application.AppEvent += new SAPbouiCOM._IApplicationEvents_AppEventEventHandler(SBO_Application_AppEvent);
+                Application.SBO_Application.ItemEvent += SBO_Application_ItemEvent;
+                oApp.Run();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static SAPbouiCOM.Form oForm;
+        public static SAPbouiCOM.Item oItem;
+        public static SAPbouiCOM.Item oOldItem;
+
+        static void SBO_Application_ItemEvent(string FormUID, ref SAPbouiCOM.ItemEvent pVal, out bool BubbleEvent)
+        {
+            //throw new NotImplementedException();
+            BubbleEvent = true;
+            if ((pVal.FormType == 146 && pVal.EventType != SAPbouiCOM.BoEventTypes.et_FORM_UNLOAD) && pVal.Before_Action == true)
+            {
+                oForm = Application.SBO_Application.Forms.GetFormByTypeAndCount(pVal.FormType, pVal.FormTypeCount);
+                if (pVal.EventType == SAPbouiCOM.BoEventTypes.et_FORM_LOAD && pVal.Before_Action == true)
+                {
+                    SAPbouiCOM.Button obt = null;
+                    oOldItem = oForm.Items.Item("2");
+                    oItem = oForm.Items.Add("btnNew", SAPbouiCOM.BoFormItemTypes.it_BUTTON);
+                    oItem.Top = oOldItem.Top;
+                    oItem.Height = oOldItem.Height;
+                    oItem.Left = oOldItem.Left + oOldItem.Width + 5;
+                    oItem.Width = oOldItem.Width + 40;
+                    obt = (SAPbouiCOM.Button)oItem.Specific;
+                    obt.Caption = "Lectora de Cheques";
+                }
+                if (pVal.ItemUID == "btnNew" & (pVal.EventType == SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED || pVal.EventType == SAPbouiCOM.BoEventTypes.et_CLICK) & pVal.Before_Action == true)
+                {
+                    Application.SBO_Application.ActivateMenuItem("Lectora_de_Cheques.Form1");
+                }
+            }
+        }
+
+        static void SBO_Application_AppEvent(SAPbouiCOM.BoAppEventTypes EventType)
+        {
+            switch (EventType)
+            {
+                case SAPbouiCOM.BoAppEventTypes.aet_ShutDown:
+                    //Exit Add-On
+                    System.Windows.Forms.Application.Exit();
+                    break;
+                case SAPbouiCOM.BoAppEventTypes.aet_CompanyChanged:
+                    break;
+                case SAPbouiCOM.BoAppEventTypes.aet_FontChanged:
+                    break;
+                case SAPbouiCOM.BoAppEventTypes.aet_LanguageChanged:
+                    break;
+                case SAPbouiCOM.BoAppEventTypes.aet_ServerTerminition:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
+
+  
