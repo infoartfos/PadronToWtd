e8fd734b0fc814d38aac6b66cda3cf33c7b3597c
Carlos V <cvalicenti@artfos.com.ar>
2026-04-21 21:45:25 -0300

    fix: :bug: lee año de la fecah desde el trimestre

--- PadronWtd.UI/Services/PeriodosService.cs
+++ PadronWtd.UI/Services/PeriodosService.cs
@@ -27,48 +27,42 @@ namespace PadronWtd.UI.Services
         {
             var rawData = await _repository.GetFechasAsync();
             var activos = rawData.ToList();
-            // var activos = rawData.Where(r => r.U_Activo == "SI").ToList();
 
             var result = new List<ComboItem>();
 
             foreach (var r in activos)
             {
-                int errores = await _padronRepository.CountErrorsAsync(r.U_Periodo, r.Year);
+                string year = $"{r.U_Desde:yyyy}";
 
-                string desc = $"{r.Year} {r.U_Periodo} ({r.U_Desde:dd/MM}-{r.U_Hasta:dd/MM})";
+                int errores = await _padronRepository.CountErrorsAsync(r.U_Periodo, year);
+
+                string desc = $"{year} {r.U_Periodo} ({r.U_Desde:dd/MM}-{r.U_Hasta:dd/MM})";
+
                 if (errores > 0)
                 {
                     desc += $" [⚠️ {errores} Errores]";
+                }
+
+                if (errores > 0 || r.U_Activo == "SI")
+                {
                     result.Add(new ComboItem
                     {
-                        Value = r.Year + " " + r.U_Periodo,
+                        Value = $"{year} {r.U_Periodo}",
                         Description = desc
                     });
-                } else
-                {
-                    if (r.U_Activo == "SI")
-                    {
-                        result.Add(new ComboItem
-                        {
-                            Value = r.Year + " " + r.U_Periodo,
-                            Description = desc
-                        });
-                    }
                 }
             }
 
             return result.OrderBy(x => x.Value).ToList();
         }
 
-
         public async Task<(DateTime? Desde, DateTime? Hasta)> GetDatesAsync(string year, string qValue)
         {
             var rawData = await _repository.GetFechasAsync();
 
             var periodo = rawData.FirstOrDefault(r =>
-                r.Year == year &&
-                r.U_Periodo == qValue
-            );
+                $"{r.U_Desde:yyyy}" == year &&
+                r.U_Periodo == qValue);
 
             if (periodo != null)
             {
